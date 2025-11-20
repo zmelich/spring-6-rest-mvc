339791a170220cf62c62f98b723b19bffb677e30
zmelich <stocek86@gmail.com>
2025-11-20 17:06:36 +0100

    Set MockMVC to return content!

--- src/test/java/guru/springframework/spring_6_rest_mvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring_6_rest_mvc/controller/BeerControllerTest.java
@@ -1,7 +1,9 @@
 package guru.springframework.spring_6_rest_mvc.controller;
 
 import guru.springframework.spring_6_rest_mvc.controller.BeerController;
+import guru.springframework.spring_6_rest_mvc.model.Beer;
 import guru.springframework.spring_6_rest_mvc.services.BeerService;
+import guru.springframework.spring_6_rest_mvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,7 +13,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.UUID;
@@ -30,11 +35,18 @@ class BeerControllerTest {
     @MockitoBean
     BeerService beerService;
 
+    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+
     @Test
     void getBeerById() throws Exception{
+        Beer testBeer = beerServiceImpl.listBeers().get(0);
+
+        given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
+
         mockMVC.perform(get("/api/v1/beer/" + UUID.randomUUID())
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
 
       //  System.out.println(beerController.getBeerById(UUID.randomUUID()));
 
