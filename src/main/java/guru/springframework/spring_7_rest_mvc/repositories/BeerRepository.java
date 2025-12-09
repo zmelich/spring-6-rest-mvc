592cee6e8c474090f1f7a121f5f4f96d9cb8c03b
zmelich <stocek86@gmail.com>
2025-12-09 15:06:47 +0100

    Adding JPA repository interfaces

---
+++ src/main/java/guru/springframework/spring_7_rest_mvc/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring_7_rest_mvc.repositories;
+
+import guru.springframework.spring_7_rest_mvc.entities.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+}
---
+++ src/main/java/guru/springframework/spring_7_rest_mvc/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring_7_rest_mvc.repositories;
+
+import guru.springframework.spring_7_rest_mvc.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+}
