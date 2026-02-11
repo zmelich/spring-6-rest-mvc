983a86d79b5e5f63873653db6ca7e8cfd7c0a071
zmelich <stocek86@gmail.com>
2026-02-11 17:26:01 +0100

    Add more test methods to BeerControllerIT to test the handling of the expected NotFoundException exception.

--- src/test/java/guru/springframework/spring_7_rest_mvc/controller/BeerControllerIT.java
+++ src/test/java/guru/springframework/spring_7_rest_mvc/controller/BeerControllerIT.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring_7_rest_mvc.controller;
 
+import guru.springframework.spring_7_rest_mvc.entities.Beer;
 import guru.springframework.spring_7_rest_mvc.model.BeerDTO;
 import guru.springframework.spring_7_rest_mvc.repositories.BeerRepository;
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import guru.springframework.spring_7_rest_mvc.model.BeerDTO;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,6 +24,23 @@ class BeerControllerIT {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testBeerNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+           beerController.getBeerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testGetBeerById() {
+        Beer beer = beerRepository.findAll().get(0);
+
+        BeerDTO dto = beerController.getBeerById(beer.getId());
+
+        assertThat(dto).isNotNull();
+
+    }
 
     @Test
     void testListBeers() {
