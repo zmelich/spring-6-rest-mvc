6e467d09ddc61011d00771d2605686cf74d37f23
zmelich <stocek86@gmail.com>
2026-02-19 16:36:26 +0100

    Modify the customer error handler to be more clean and lean.

--- src/test/java/guru/springframework/spring_7_rest_mvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring_7_rest_mvc/controller/BeerControllerTest.java
@@ -84,6 +84,7 @@ class BeerControllerTest {
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(beerDTO)))
+                .andExpect(jsonPath("$.length()",is(2)))
                 .andExpect(status().isBadRequest()).andReturn();
 
         System.out.println(mvcResult.getResponse().getContentAsString());
