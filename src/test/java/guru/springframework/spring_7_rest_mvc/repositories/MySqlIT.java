1301964af08543d01271e1729348f21f551f46ed
zmelich <stocek86@gmail.com>
2026-03-16 17:44:34 +0100

    Start using Maven Failsafe plugin for integration tests. I added Maven Surefire plugin on my own. (maybe it will be removed later)

--- src/test/java/guru/springframework/spring_7_rest_mvc/repositories/MySqlIT.java
+++ src/test/java/guru/springframework/spring_7_rest_mvc/repositories/MySqlIT.java
@@ -11,8 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -22,16 +20,14 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.mysql.MySQLContainer;
 
 
-import javax.sql.DataSource;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 @Testcontainers
 @SpringBootTest
 @ActiveProfiles("localmysql")
-public class MySqlTest {
+public class MySqlIT {
 
     @Container
     @ServiceConnection
