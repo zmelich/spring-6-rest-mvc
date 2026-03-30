a08dfc739c9a7be5eb265dc8952e8c42a450eb31
zmelich <stocek86@gmail.com>
2026-03-30 16:56:03 +0200

    Adding OpenCSV to the project and start using its annotation for property mapping.

--- src/main/java/guru/springframework/spring_7_rest_mvc/model/BeerCSVRecord.java
+++ src/main/java/guru/springframework/spring_7_rest_mvc/model/BeerCSVRecord.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring_7_rest_mvc.model;
 
 
+import com.opencsv.bean.CsvBindByName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,20 +14,49 @@ Created by Zsolt Melich (BT - IVR team)
 @NoArgsConstructor
 public class BeerCSVRecord {
 
+    @CsvBindByName
     private Integer row;
+
+    @CsvBindByName(column = "count.x")
     private Integer count;
+
+    @CsvBindByName
     private String abv;
+
+    @CsvBindByName
     private String ibu;
+
+    @CsvBindByName
     private Integer id;
+
+    @CsvBindByName
     private String beer;
+
+    @CsvBindByName
     private String style;
+
+    @CsvBindByName(column = "brewery_id")
     private Integer breweryId;
+
+    @CsvBindByName
     private Float ounces;
+
+    @CsvBindByName
     private String style2;
+
+    @CsvBindByName(column = "count.y")
     private Integer count_y;
+
+    @CsvBindByName
     private String brewery;
+
+    @CsvBindByName
     private String city;
+
+    @CsvBindByName
     private String state;
+
+    @CsvBindByName
     private String label;
 
 }
