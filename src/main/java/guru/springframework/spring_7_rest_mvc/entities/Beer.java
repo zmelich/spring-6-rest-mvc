b8b74ed1e97620d79bf96cb64c9060a3b5df7104
zmelich <stocek86@gmail.com>
2026-03-31 16:55:46 +0200

    Fix the issue with the CreatedDate and UpdateDate columns in the database during the CSV fill-up. (add @CreationTimestamp and @UpdateTimestamp annotations to the Beer entity.)

--- src/main/java/guru/springframework/spring_7_rest_mvc/entities/Beer.java
+++ src/main/java/guru/springframework/spring_7_rest_mvc/entities/Beer.java
@@ -17,7 +17,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.type.SqlTypes;
 
@@ -62,6 +64,9 @@ public class Beer {
     @Positive
     private BigDecimal price;
 
+    @CreationTimestamp
     private LocalDateTime createdDate;
+
+    @UpdateTimestamp
     private LocalDateTime updateDate;
 }
