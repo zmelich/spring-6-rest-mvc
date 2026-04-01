2da58c8968c4d2fb95adf84910bb757672f7242d
zmelich <stocek86@gmail.com>
2026-04-01 15:09:43 +0200

    Fix things that made some tests fail after our CSV data population changes.

--- src/test/java/guru/springframework/spring_7_rest_mvc/bootstrap/BootStrapDataTest.java
+++ src/test/java/guru/springframework/spring_7_rest_mvc/bootstrap/BootStrapDataTest.java
@@ -3,15 +3,18 @@ package guru.springframework.spring_7_rest_mvc.bootstrap;
 import guru.springframework.spring_7_rest_mvc.repositories.BeerRepository;
 import guru.springframework.spring_7_rest_mvc.repositories.CustomerRepository;
 import guru.springframework.spring_7_rest_mvc.services.BeerCsvService;
+import guru.springframework.spring_7_rest_mvc.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootStrapDataTest {
 
     @Autowired
@@ -34,7 +37,7 @@ class BootStrapDataTest {
     void testRun() throws Exception {
         bootStrapData.run(null);
 
-        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(beerRepository.count()).isEqualTo(2413);
         assertThat(customerRepository.count()).isEqualTo(3);
 
     }
