1427fda75c718554ce14740d52dce5069b08d6ee
zmelich <stocek86@gmail.com>
2026-04-15 17:03:33 +0200

    Complete the implementation for the beerName filtering in listBeers(). A fix also added to pass beerName instead of string "beerName".

--- src/main/java/guru/springframework/spring_7_rest_mvc/services/BeerServiceJPA.java
+++ src/main/java/guru/springframework/spring_7_rest_mvc/services/BeerServiceJPA.java
@@ -34,7 +34,7 @@ public class BeerServiceJPA implements BeerService {
 
         List<Beer> beerList;
 
-        if (StringUtils.hasText("beerName")) {
+        if (StringUtils.hasText(beerName)) {
             beerList = listBeersByName(beerName);
         }
         else
@@ -55,7 +55,7 @@ public class BeerServiceJPA implements BeerService {
     }
 
     List<Beer> listBeersByName(String beerName){
-        return new ArrayList<>();
+        return new ArrayList<>(beerRepository.findAllByBeerNameIsLikeIgnoreCase("%" + beerName + "%"));
     }
 
     @Override
--- src/test/java/guru/springframework/spring_7_rest_mvc/controller/BeerControllerIT.java
+++ src/test/java/guru/springframework/spring_7_rest_mvc/controller/BeerControllerIT.java
@@ -64,7 +64,7 @@ class BeerControllerIT {
         mockMvc.perform(get(BeerController.BEER_PATH)
                 .queryParam("beerName", "IPA"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.size()",is(100)));
+                .andExpect(jsonPath("$.size()",is(336)));
     }
 
 
